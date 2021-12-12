2882d673300ba2cce6fe3a1971e3db3424d302ad
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-12 11:30:05 -0800

    Add display, operators, digits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				Hello, Cal
-			</header>
+            <div className="calculator">
+                <div className="display">
+                    <span>(0)</span> 0
+                </div>
+                <div className="operators">
+                    <button>/</button>
+                    <button>*</button>
+                    <button>+</button>
+                    <button>-</button>
+                    <button>DEL</button>
+                </div>
+                <div className="digits">
+                    <button>0</button>
+                    <button>.</button>
+                    <button>=</button>
+                </div>
+            </div>  
 		</div>
 	);
 }
